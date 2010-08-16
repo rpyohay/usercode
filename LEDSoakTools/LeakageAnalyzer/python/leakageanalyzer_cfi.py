6a4f02969a76f7731caeaeec08f86d1a86dc68a4
yohay <>
2010-08-16 16:17:51 +0000

    This commit was manufactured by cvs2svn to create tag 'V1-working'.
    Sprout from master 2010-08-16 16:17:50 UTC yohay 'first commit'
    Delete:
        GMSBTools/CutAnalyzer/BuildFile
        GMSBTools/CutAnalyzer/crab.cfg
        GMSBTools/CutAnalyzer/cutanalyzer_cfg.py
        GMSBTools/CutAnalyzer/cutanalyzer_cfg_gg_data.py
        GMSBTools/CutAnalyzer/python/cutanalyzer_cfi.py
        GMSBTools/CutAnalyzer/src/CutAnalyzer.cc
        GMSBTools/EventSelection/BuildFile
        GMSBTools/EventSelection/interface/EventSelector.h
        GMSBTools/EventSelection/python/eventselection_cfi.py
        GMSBTools/EventSelection/src/EventSelector.cc
        GMSBTools/SampleMaker/BuildFile
        GMSBTools/SampleMaker/interface/SampleMaker.h
        GMSBTools/SampleMaker/python/samplemaker_cfi.py
        GMSBTools/SampleMaker/src/SampleMaker.cc
        LEDGapTools/LMMapMaker/BuildFile
        LEDGapTools/LMMapMaker/lmmapmaker_cfg.py
        LEDGapTools/LMMapMaker/python/__init__.py
        LEDGapTools/LMMapMaker/python/lmmapmaker_cfi.py
        LEDGapTools/LMMapMaker/src/LMMapMaker.cc
        LEDGapTools/TEFilter/BuildFile
        LEDGapTools/TEFilter/python/__init__.py
        LEDGapTools/TEFilter/python/tefilter_cfi.py
        LEDGapTools/TEFilter/src/TEFilter.cc
        LEDGapTools/TEFilter/tefilter_cfg_118333.py
        LEDGapTools/TEFilter/tefilter_cfg_118333.sh
        LEDGapTools/TEFilter/tefilter_cfg_118346.py
        LEDGapTools/TEFilter/tefilter_cfg_118346.sh
        LEDGapTools/TEFilter/tefilter_cfg_118358.py
        LEDGapTools/TEFilter/tefilter_cfg_118358.sh
        LEDGapTools/TEFilter/tefilter_cfg_118743.py
        LEDGapTools/TEFilter/tefilter_cfg_118743.sh
        LEDSoakTools/LeakageAnalyzer/BuildFile
        LEDSoakTools/LeakageAnalyzer/leakageanalyzer_cfg.py
        LEDSoakTools/LeakageAnalyzer/python/__init__.py
        LEDSoakTools/LeakageAnalyzer/python/leakageanalyzer_cfi.py
        LEDSoakTools/LeakageAnalyzer/src/LeakageAnalyzer.cc
        LEDSoakTools/LeakageFilter/BuildFile
        LEDSoakTools/LeakageFilter/leakagefilter_cfg.py
        LEDSoakTools/LeakageFilter/leakagefilter_cfg.sh
        LEDSoakTools/LeakageFilter/python/__init__.py
        LEDSoakTools/LeakageFilter/python/leakagefilter_cfi.py
        LEDSoakTools/LeakageFilter/src/LeakageFilter.cc
        LEDSoakTools/OrbitNumberAnalyzer/BuildFile
        LEDSoakTools/OrbitNumberAnalyzer/orbitnumberanalyzer_cfg.py
        LEDSoakTools/OrbitNumberAnalyzer/python/orbitnumberanalyzer_cfi.py
        LEDSoakTools/OrbitNumberAnalyzer/src/OrbitNumberAnalyzer.cc
        PATTools/PATVerifier/BuildFile
        PATTools/PATVerifier/patverifier_cfg.py
        PATTools/PATVerifier/patverifier_cfg.sh
        PATTools/PATVerifier/python/patverifier_cfi.py
        PATTools/PATVerifier/src/PATVerifier.cc
        prompt_feedback/CalibrationSequenceAnalyzer/BuildFile
        prompt_feedback/CalibrationSequenceAnalyzer/batch_jobs/submission/submit.sh
        prompt_feedback/CalibrationSequenceAnalyzer/batch_jobs/submission/submit_1_job.sh
        prompt_feedback/CalibrationSequenceAnalyzer/cfg/calibrationsequenceanalyzer_cfg.py
        prompt_feedback/CalibrationSequenceAnalyzer/python/__init__.py
        prompt_feedback/CalibrationSequenceAnalyzer/python/calibrationsequenceanalyzer_cfi.py
        prompt_feedback/CalibrationSequenceAnalyzer/sh/calibrationsequenceanalyzer_cfg.sh
        prompt_feedback/CalibrationSequenceAnalyzer/src/CalibrationSequenceAnalyzer.cc
        prompt_feedback/analyze_LED_gap_events.C
        prompt_feedback/run_analyze_LED_gap_events.C

--- LEDSoakTools/LeakageAnalyzer/python/leakageanalyzer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-LeakageAnalyzer = cms.EDAnalyzer('LeakageAnalyzer',
-                                 EEDigiCollection = cms.InputTag("ecalEBunpacker","eeDigis"),
-                                 outName = cms.string("/afs/cern.ch/user/y/yohay/scratch0/leakage_121620_random-triggered_filtered.root")
-)
