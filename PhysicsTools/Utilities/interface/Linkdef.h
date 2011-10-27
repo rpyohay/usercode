76a9851474490ae1f6a9103c888c121f6b45062f
yohay <>
2011-10-27 09:50:39 +0000

    PU reweighting tools for MC

---
+++ PhysicsTools/Utilities/interface/Linkdef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#include "LumiReweightingStandAlone.h"
+
+#pragma link C++ class reweight::PoissonMeanShifter+;
+#pragma link C++ class reweight::LumiReWeighting+;
+#endif // __CINT__
