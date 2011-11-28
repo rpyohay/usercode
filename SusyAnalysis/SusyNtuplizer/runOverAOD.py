9873df792defd4cc6f1961b902acd066936e53db
yohay <>
2011-11-28 10:06:17 +0000

    added Type-I MET corrections

--- SusyAnalysis/SusyNtuplizer/runOverAOD.py
+++ SusyAnalysis/SusyNtuplizer/runOverAOD.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 # change this to 0 if you run on MC files
-realData = 0
+realData = 1
 
 process = cms.Process("RA3")
 
 process.load('FWCore/MessageService/MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 500
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
                             noEventSort = cms.untracked.bool(True),
@@ -22,35 +22,62 @@ process.load('Configuration.StandardSequences.Reconstruction_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
-if realData:
-    process.source.fileNames = cms.untracked.vstring('/store/data/Run2011A/DoubleElectron/AOD/May10ReReco-v1/0000/003D325C-547B-E011-81D4-001A928116C2.root')
-    process.GlobalTag.globaltag = 'GR_R_42_V12::All'
-else:
-    process.source.fileNames = cms.untracked.vstring('dcap:///pnfs/cms/WAX/resilient/lpcpjm/PrivateMC/BinoSignalPoints_5_7_11/reco/1250_1200_225/reco_1250_1200_225_1.root')
-    #process.source.fileNames = cms.untracked.vstring('/store/relval/CMSSW_4_2_3/RelValPhotonJets_Pt_10/GEN-SIM-RECO/START42_V12-v2/0062/AA819E58-077B-E011-8C9C-0018F3D095FC.root')
-    process.GlobalTag.globaltag = 'START42_V12::All'
-
 process.load("JetMETCorrections.Configuration.DefaultJEC_cff")
+process.load("JetMETCorrections.Type1MET.pfMETCorrections_cff")
+process.load("JetMETCorrections/Configuration/JetCorrectionServices_cff")
+process.load("JetMETCorrections/Type1MET/caloMETCorrections_cff")
+process.metAnalysisSequence = cms.Sequence(process.producePFMETCorrections*
+                                           process.produceCaloMETCorrections)
 
 # calculate rho & jetArea
-process.load('RecoJets.Configuration.RecoPFJets_cff')
-process.kt6PFJets.doRhoFastjet = True
-process.ak5PFJets.doAreaFastjet = True
-
+#process.load('RecoJets.Configuration.RecoPFJets_cff')
+#process.kt6PFJets.doRhoFastjet = True
+#process.ak5PFJetsWithFastjetArea = process.ak5PFJets.clone()
+#process.ak5PFJetsWithFastjetArea.doAreaFastjet = True
 
 # SusyNtuplizer options
 process.load("SusyAnalysis.SusyNtuplizer.susyNtuplizer_cfi")
 process.susyNtuplizer.debugLevel = cms.int32(0)
+#process.susyNtuplizer.outputFileName = cms.string(
+#    "/data2/yohay/Summer11_TT_debug/Type-I_MET_debug_ntuple.root"
+#    )
 
-# JEC
-process.jet = cms.Sequence(
-    process.kt6PFJets * process.ak5PFJets *
-    # CaloJets
-    process.ak5CaloJetsL2L3 * process.ak5CaloJetsL1L2L3 *
-    # PFJets
-    process.ak5PFJetsL2L3 * process.ak5PFJetsL1FastL2L3 *
-    # JPTJets
-    process.ak5JPTJetsL2L3 * process.ak5JPTJetsL1L2L3
+if realData:
+    process.source.fileNames = cms.untracked.vstring(
+        'file:/data2/yohay/Photon_Aug5ReReco.root'
+        )
+    process.GlobalTag.globaltag = 'GR_R_42_V19::All'
+    process.pfJetMETcorr.jetCorrLabel = cms.string("ak5PFL1FastL2L3Residual")
+    process.caloJetMETcorr.jetCorrLabel = cms.string("ak5CaloL2L3Residual")
+    # JEC for data
+    process.jet = cms.Sequence(
+#        process.kt6PFJets * process.ak5PFJetsWithFastjetArea *
+        # CaloJets
+        process.ak5CaloJetsL2L3Residual * process.ak5CaloJetsL1L2L3Residual *
+        # PFJets
+        process.ak5PFJetsL2L3Residual * process.ak5PFJetsL1FastL2L3Residual
+    )
+else:
+    process.source.fileNames = cms.untracked.vstring(
+        'file:/data2/yohay/Summer11_TT_debug/26F2AB20-348C-E011-BCF1-0017A4770400.root'
+        )
+    process.GlobalTag.globaltag = 'START42_V13::All'
+    process.pfJetMETcorr.jetCorrLabel = cms.string("ak5PFL1FastL2L3")
+    process.caloJetMETcorr.jetCorrLabel = cms.string("ak5CaloL2L3")
+    # JEC for MC
+    process.jet = cms.Sequence(
+#        process.kt6PFJets * process.ak5PFJetsWithFastjetArea *
+        # CaloJets
+        process.ak5CaloJetsL2L3 * process.ak5CaloJetsL1L2L3 *
+        # PFJets
+        process.ak5PFJetsL2L3 * process.ak5PFJetsL1FastL2L3
     )
 
-process.p = cms.Path( process.jet * process.susyNtuplizer )
+#process.out = cms.OutputModule("PoolOutputModule",
+#                               fileName = cms.untracked.string(
+#    "/data2/yohay/Summer11_TT_debug/Type-I_MET_debug_AOD.root"
+#    )
+#                               )
+
+process.p = cms.Path( process.metAnalysisSequence * process.jet * process.susyNtuplizer )
+#process.e = cms.EndPath(process.out)
