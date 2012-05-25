de4bbf9e51fcadd4eafea8d7bb6901188478857f
yohay <>
2012-05-25 12:10:02 +0000

    added ability to process old scans; added calculation of relative statistical error on acceptance for all models

--- SusyAnalysis/SusyNtuplizer/macro/runAcceptances.C
+++ SusyAnalysis/SusyNtuplizer/macro/runAcceptances.C
@@ -30,31 +30,62 @@
   const Double_t mBino2StepSize = (winoBinoMaxBin.second - winoBinoMinBin.second)/(Double_t)winoBinoNBins.second;
   const Double_t mBino2StepSizeOver2 = mBino2StepSize/2.0;
 
+  //binolikegrid2 scan binning
+  const pair<Int_t, Int_t> squarkGluinoBinoOldNBins(21, 21);
+  const pair<Double_t, Double_t> squarkGluinoBinoOldMinBin(360.0, 360.0);
+  const pair<Double_t, Double_t> squarkGluinoBinoOldMaxBin(2040.0, 2040.0);
+  const Double_t mSquark2StepSize = 
+    (squarkGluinoBinoOldMaxBin.first - squarkGluinoBinoOldMinBin.first)/(Double_t)squarkGluinoBinoOldNBins.first;
+  const Double_t mSquark2StepSizeOver2 = mSquark2StepSize/2.0;
+  const Double_t mGluino3StepSize = 
+    (squarkGluinoBinoOldMaxBin.second - squarkGluinoBinoOldMinBin.second)/(Double_t)squarkGluinoBinoOldNBins.second;
+  const Double_t mGluino3StepSizeOver2 = mGluino3StepSize/2.0;
+
+  //winolikegrid2 scan binning
+  const pair<Int_t, Int_t> squarkGluinoWinoOldNBins(21, 21);
+  const pair<Double_t, Double_t> squarkGluinoWinoOldMinBin(360.0, 360.0);
+  const pair<Double_t, Double_t> squarkGluinoWinoOldMaxBin(2040.0, 2040.0);
+  const Double_t mSquark3StepSize = 
+    (squarkGluinoWinoOldMaxBin.first - squarkGluinoWinoOldMinBin.first)/(Double_t)squarkGluinoWinoOldNBins.first;
+  const Double_t mSquark3StepSizeOver2 = mSquark3StepSize/2.0;
+  const Double_t mGluino4StepSize = 
+    (squarkGluinoWinoOldMaxBin.second - squarkGluinoWinoOldMinBin.second)/(Double_t)squarkGluinoWinoOldNBins.second;
+  const Double_t mGluino4StepSizeOver2 = mGluino4StepSize/2.0;
+
+  //binochigrids scan binning
+  const pair<Int_t, Int_t> binoGluinoOldNBins(10, 24);
+  const pair<Double_t, Double_t> binoGluinoOldMinBin(100, 120);
+  const pair<Double_t, Double_t> binoGluinoOldMaxBin(1100, 2040);
+  const Double_t mBino3StepSize = (binoGluinoOldMaxBin.first - binoGluinoOldMinBin.first)/(Double_t)binoGluinoOldNBins.first;
+  const Double_t mBino3StepSizeOver2 = mBino3StepSize/2.0;
+  const Double_t mGluino5StepSize = (binoGluinoOldMaxBin.second - binoGluinoOldMinBin.second)/(Double_t)binoGluinoOldNBins.second;
+  const Double_t mGluino5StepSizeOver2 = mGluino5StepSize/2.0;
+
   //gsq_B acceptance files
   vector<string> squarkGluinoAccFiles;
-  string squarkGluinoPath("/uscms_data/d2/rpyohay/Spectra_gsq_B/");
-  for (UInt_t mSquark = (UInt_t)(squarkGluinoMinBin.first + mSquarkStepSizeOver2); 
-       mSquark <= (UInt_t)(squarkGluinoMaxBin.first - mSquarkStepSizeOver2); mSquark+=(UInt_t)mSquarkStepSize) {
-    for (UInt_t mGluino = (UInt_t)(squarkGluinoMinBin.second + mGluino1StepSizeOver2); 
-	 mGluino <= (UInt_t)(squarkGluinoMaxBin.second - mGluino1StepSizeOver2); mGluino+=(UInt_t)mGluino1StepSize) {
-      stringstream file;
-      file << squarkGluinoPath << "acceptance_" << mSquark << "_" << mGluino << "_375.root";
-      squarkGluinoAccFiles.push_back(file.str().c_str());
-    }
-  }
+//   string squarkGluinoPath("/uscms_data/d2/rpyohay/Spectra_gsq_B/");
+//   for (UInt_t mSquark = (UInt_t)(squarkGluinoMinBin.first + mSquarkStepSizeOver2); 
+//        mSquark <= (UInt_t)(squarkGluinoMaxBin.first - mSquarkStepSizeOver2); mSquark+=(UInt_t)mSquarkStepSize) {
+//     for (UInt_t mGluino = (UInt_t)(squarkGluinoMinBin.second + mGluino1StepSizeOver2); 
+// 	 mGluino <= (UInt_t)(squarkGluinoMaxBin.second - mGluino1StepSizeOver2); mGluino+=(UInt_t)mGluino1StepSize) {
+//       stringstream file;
+//       file << squarkGluinoPath << "acceptance_" << mSquark << "_" << mGluino << "_375.root";
+//       squarkGluinoAccFiles.push_back(file.str().c_str());
+//     }
+//   }
 
   //gB acceptance files
   vector<string> binoGluinoAccFiles;
-  string binoGluinoPath("/uscms_data/d2/rpyohay/Spectra_gB/");
-  for (UInt_t mBino = (UInt_t)(binoGluinoMinBin.first + mBino1StepSizeOver2); 
-       mBino <= (UInt_t)(binoGluinoMaxBin.first - mBino1StepSizeOver2); mBino+=(UInt_t)mBino1StepSize) {
-    for (UInt_t mGluino = (UInt_t)(binoGluinoMinBin.second + mGluino2StepSizeOver2); 
-	 mGluino <= (UInt_t)(binoGluinoMaxBin.second - mGluino2StepSizeOver2); mGluino+=(UInt_t)mGluino2StepSize) {
-      stringstream file;
-      file << binoGluinoPath << "acceptance_2000_" << mGluino << "_" << mBino << ".root";
-      binoGluinoAccFiles.push_back(file.str().c_str());
-    }
-  }
+//   string binoGluinoPath("/uscms_data/d2/rpyohay/Spectra_gB/");
+//   for (UInt_t mBino = (UInt_t)(binoGluinoMinBin.first + mBino1StepSizeOver2); 
+//        mBino <= (UInt_t)(binoGluinoMaxBin.first - mBino1StepSizeOver2); mBino+=(UInt_t)mBino1StepSize) {
+//     for (UInt_t mGluino = (UInt_t)(binoGluinoMinBin.second + mGluino2StepSizeOver2); 
+// 	 mGluino <= (UInt_t)(binoGluinoMaxBin.second - mGluino2StepSizeOver2); mGluino+=(UInt_t)mGluino2StepSize) {
+//       stringstream file;
+//       file << binoGluinoPath << "acceptance_2000_" << mGluino << "_" << mBino << ".root";
+//       binoGluinoAccFiles.push_back(file.str().c_str());
+//     }
+//   }
 
   //WB acceptance files
   vector<string> winoBinoAccFiles;
@@ -69,8 +100,95 @@
 //     }
 //   }
 
+  //binolikegrid2 acceptance files
+  vector<string> squarkGluinoBinoOldAccFiles;
+  string squarkGluinoBinoOldPath("/uscms_data/d2/rpyohay/binolikegrid2/");
+  for (UInt_t mSquark = (UInt_t)(squarkGluinoBinoOldMinBin.first + mSquark2StepSizeOver2); 
+       mSquark <= (UInt_t)(squarkGluinoBinoOldMaxBin.first - mSquark2StepSizeOver2); mSquark+=(UInt_t)mSquark2StepSize) {
+    for (UInt_t mGluino = (UInt_t)(squarkGluinoBinoOldMinBin.second + mGluino3StepSizeOver2); 
+	 mGluino <= (UInt_t)(squarkGluinoBinoOldMaxBin.second - mGluino3StepSizeOver2); mGluino+=(UInt_t)mGluino3StepSize) {
+      stringstream file;
+      file << squarkGluinoBinoOldPath << "acceptance_" << mSquark << "_" << mGluino << "_375.root";
+      squarkGluinoBinoOldAccFiles.push_back(file.str().c_str());
+    }
+  }
+
+  //winolikegrid2 acceptance files
+  vector<string> squarkGluinoWinoOldAccFiles;
+  string squarkGluinoWinoOldPath("/uscms_data/d2/rpyohay/winolikegrid2/");
+  for (UInt_t mSquark = (UInt_t)(squarkGluinoWinoOldMinBin.first + mSquark3StepSizeOver2); 
+       mSquark <= (UInt_t)(squarkGluinoWinoOldMaxBin.first - mSquark3StepSizeOver2); mSquark+=(UInt_t)mSquark3StepSize) {
+    for (UInt_t mGluino = (UInt_t)(squarkGluinoWinoOldMinBin.second + mGluino4StepSizeOver2); 
+	 mGluino <= (UInt_t)(squarkGluinoWinoOldMaxBin.second - mGluino4StepSizeOver2); mGluino+=(UInt_t)mGluino4StepSize) {
+      stringstream file;
+      file << squarkGluinoWinoOldPath << "acceptance_" << mSquark << "_" << mGluino << "_375.root";
+      squarkGluinoWinoOldAccFiles.push_back(file.str().c_str());
+    }
+  }
+
+  //binochigrids acceptance files
+  vector<string> binoGluinoOldAccFiles;
+  string binoGluinoOldPath("/uscms_data/d2/rpyohay/binochigrids/");
+  for (UInt_t mBino = (UInt_t)(binoGluinoOldMinBin.first + mBino3StepSizeOver2); 
+       mBino <= (UInt_t)(binoGluinoOldMaxBin.first - mBino3StepSizeOver2); mBino+=(UInt_t)mBino3StepSize) {
+    for (UInt_t mGluino = (UInt_t)(binoGluinoOldMinBin.second + mGluino5StepSizeOver2); 
+	 mGluino <= (UInt_t)(binoGluinoOldMaxBin.second - mGluino5StepSizeOver2); mGluino+=(UInt_t)mGluino5StepSize) {
+      stringstream file;
+      file << binoGluinoOldPath << "acceptance_2500_" << mGluino << "_" << mBino << ".root";
+      binoGluinoOldAccFiles.push_back(file.str().c_str());
+    }
+  }
+
   //run acceptances
-  writeAcceptanceFile("/uscms_data/d2/rpyohay/acc_squarkGluino_binoGluino_METGeq50GeV.root", squarkGluinoNBins, binoGluinoNBins, 
-		      winoBinoNBins, squarkGluinoMinBin, binoGluinoMinBin, winoBinoMinBin, squarkGluinoMaxBin, binoGluinoMaxBin, 
-		      winoBinoMaxBin, squarkGluinoAccFiles, binoGluinoAccFiles, winoBinoAccFiles);
+  writeAcceptanceFile("/uscms_data/d2/rpyohay/acc_old.root", squarkGluinoNBins, binoGluinoNBins, winoBinoNBins, squarkGluinoBinoOldNBins, 
+		      squarkGluinoWinoOldNBins, binoGluinoOldNBins, squarkGluinoMinBin, binoGluinoMinBin, winoBinoMinBin, 
+		      squarkGluinoBinoOldMinBin, squarkGluinoWinoOldMinBin, binoGluinoOldMinBin, squarkGluinoMaxBin, binoGluinoMaxBin, 
+		      winoBinoMaxBin, squarkGluinoBinoOldMaxBin, squarkGluinoWinoOldMaxBin, binoGluinoOldMaxBin, squarkGluinoAccFiles, 
+		      binoGluinoAccFiles, winoBinoAccFiles, squarkGluinoBinoOldAccFiles, squarkGluinoWinoOldAccFiles, 
+		      binoGluinoOldAccFiles);
+
+  //MET bins
+  vector<float> METBins;
+  METBins.push_back(50.0);
+  METBins.push_back(60.0);
+  METBins.push_back(70.0);
+  METBins.push_back(80.0);
+  METBins.push_back(100.0);
+
+//   //print ranges of nGG rel. stat. error
+//   sortNGGRelStatErr(squarkGluinoBinoOldAccFiles, squarkGluinoWinoOldAccFiles, binoGluinoOldAccFiles, METBins);
+
+  //signal files for limit setting
+  vector<string> outputROOTFiles;
+  outputROOTFiles.
+    push_back("/uscms/home/rpyohay/UserCode/LPCPJM/LimitSetting_Jan2012/inputHists.20120520/signal_contamination_bino_chi0375.root");
+  outputROOTFiles.
+    push_back("/uscms/home/rpyohay/UserCode/LPCPJM/LimitSetting_Jan2012/inputHists.20120520/signal_contamination_wino_chi0375.root");
+  outputROOTFiles.
+    push_back("/uscms/home/rpyohay/UserCode/LPCPJM/LimitSetting_Jan2012/inputHists.20120520/signal_contamination_bino_chi0.root");
+
+  //no. generated events files for limit setting
+  vector<string> outputTxtFiles;
+  outputTxtFiles.
+    push_back("/uscms/home/rpyohay/UserCode/LPCPJM/LimitSetting_Jan2012/xsecdat/mcAccMap_bino_mN375_20120520.dat");
+  outputTxtFiles.
+    push_back("/uscms/home/rpyohay/UserCode/LPCPJM/LimitSetting_Jan2012/xsecdat/mcAccMap_wino_mN375_20120520.dat");
+  outputTxtFiles.
+    push_back("/uscms/home/rpyohay/UserCode/LPCPJM/LimitSetting_Jan2012/xsecdat/mcAccMap_bino_mNScan_20120520.dat");
+
+  //vector of vectors of acceptance files names (1 vector for each grid)
+  vector<vector<string> > accFiles;
+  accFiles.push_back(squarkGluinoBinoOldAccFiles);
+  accFiles.push_back(squarkGluinoWinoOldAccFiles);
+  accFiles.push_back(binoGluinoOldAccFiles);
+
+  //scans
+  vector<string> scans;
+  scans.push_back("gsq_B");
+  scans.push_back("gsq_W");
+  scans.push_back("gB");
+
+//   makeFilesForLimitSetting(outputROOTFiles, accFiles, scans, "nojet");
+
+  makeNGenFiles(outputTxtFiles, accFiles, scans);
 }
