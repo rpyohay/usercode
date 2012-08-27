25d08960e66630a8062497837cc0f113e762e38b
yohay <>
2012-08-27 15:08:45 +0000

    updated to keep TriggerResults and all HPS tau discriminator collections

--- BoostedTauAnalysis/TauSkimmer/test/test_tauSkim.py
+++ BoostedTauAnalysis/TauSkimmer/test/test_tauSkim.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("SKIM")
-## process = cms.Process("HADTAUSKIM")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
@@ -10,7 +9,6 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
 
-#process.options = cms.untracked.PSet(SkipEvent = cms.untracked.vstring('ProductNotFound'))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
@@ -30,7 +28,7 @@ process.source.inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMCo
 
 process.HadronicTauDecayFinder = cms.EDFilter('HadronicTauDecayFinder',
                                               genParticleTag = cms.InputTag('genParticles'),
-                                              momPDGID = cms.uint32(23),
+                                              momPDGID = cms.int32(23),
                                               maxAbsEta = cms.double(2.3)
                                               )
 
@@ -50,7 +48,6 @@ process.output = cms.OutputModule(
     "drop recoPFJets_kt6PFJets_*_*",
     "drop *_fixedGridRho*_*_*",
     "drop *_hfEMClusters_*_*",
-##     "drop *_tevMuons_*_*",
     "drop *_eid*_*_*",
     "drop *_muonMETValueMapProducer_muCorrData_*",
     "drop *_muons_muonShowerInformation_*",
@@ -62,7 +59,6 @@ process.output = cms.OutputModule(
     "drop recoCaloClusters_*_*_*",
     "drop recoPreshowerCluster*_*_*_*",
     "drop *_hfRecoEcalCandidate_*_*",
-##     "drop *_regionalCosmicTracks_*_*",
     "drop *_generalV0Candidates_*_*",
     "drop *_selectDigi_*_*",
     "drop *_*BJetTags*_*_*",
@@ -90,38 +86,12 @@ process.output = cms.OutputModule(
     "drop recoMETs_*_*_*",
     "drop *_photons_*_*",
     "drop *_photonCore_*_*",
-##     "drop *_*osmicMuons*_*_*",
-##     "drop *_uncleanedOnly*_*_*",
-    "drop *_TriggerResults_*_RECO",
     "drop *_ak5PFJetsRecoTauPiZeros_*_RECO",
-    "drop *_hpsPFTauDiscriminationByDecayModeFinding_*_RECO",
-    "drop *_hpsPFTauDiscriminationByLooseChargedIsolation_*_RECO",
-    "drop *_hpsPFTauDiscriminationByLooseCombinedIsolationDBSumPtCorr_*_*",
-    "drop *_hpsPFTauDiscriminationByLooseElectronRejection_*_*",
-    "drop *_hpsPFTauDiscriminationByLooseIsolation_*_*",
-    "drop *_hpsPFTauDiscriminationByLooseIsolationDBSumPtCorr_*_*",
-    "drop *_hpsPFTauDiscriminationByLooseMuonRejection_*_RECO",
-    "drop *_hpsPFTauDiscriminationByMVAElectronRejection_*_RECO",
-    "drop *_hpsPFTauDiscriminationByMediumChargedIsolation_*_RECO",
-    "drop *_hpsPFTauDiscriminationByMediumCombinedIsolationDBSumPtCorr_*_*",
-    "drop *_hpsPFTauDiscriminationByMediumElectronRejection_*_*",
-    "drop *_hpsPFTauDiscriminationByMediumIsolation_*_*",
-    "drop *_hpsPFTauDiscriminationByMediumIsolationDBSumPtCorr_*_*",
-    "drop *_hpsPFTauDiscriminationByMediumMuonRejection_*_RECO",
-    "drop *_hpsPFTauDiscriminationByRawChargedIsolationDBSumPtCorr_*_RECO",
-    "drop *_hpsPFTauDiscriminationByRawCombinedIsolationDBSumPtCorr_*_RECO",
-    "drop *_hpsPFTauDiscriminationByRawGammaIsolationDBSumPtCorr_*_RECO",
-    "drop *_hpsPFTauDiscriminationByTightChargedIsolation_*_RECO",
-    "drop *_hpsPFTauDiscriminationByTightCombinedIsolationDBSumPtCorr_*_*",
-    "drop *_hpsPFTauDiscriminationByTightElectronRejection_*_*",
-    "drop *_hpsPFTauDiscriminationByTightIsolation_*_*",
-    "drop *_hpsPFTauDiscriminationByTightIsolationDBSumPtCorr_*_*",
-    "drop *_hpsPFTauDiscriminationByTightMuonRejection_*_RECO",
-    "drop *_hpsPFTauDiscriminationByVLooseChargedIsolation_*_RECO",
-    "drop *_hpsPFTauDiscriminationByVLooseCombinedIsolationDBSumPtCorr_*_*",
-    "drop *_hpsPFTauDiscriminationByVLooseIsolation_*_*",
-    "drop *_hpsPFTauDiscriminationByVLooseIsolationDBSumPtCorr_*_*",
+    "drop *_ak5PFJetsRecoTauPiZeros_*_HLT",
+    "drop *_hpsPFTauDiscrimination*_*_RECO",
+    "drop *_hpsPFTauDiscrimination*_*_HLT",
     "drop *_hpsPFTauProducer_*_RECO",
+    "drop *_hpsPFTauProducer_*_HLT",
     "drop *_recoTauAK5PFJets08Region_*_SKIM",
     "drop *_ak5PFJetTracksAssociatorAtVertex_*_SKIM",
     "drop *_ak5PFJetsLegacyHPSPiZeros_*_SKIM",
@@ -132,12 +102,13 @@ process.output = cms.OutputModule(
     "drop *_hpsPFTauProducerSansRefs_*_SKIM",
     "drop *_pfRecoTauTagInfoProducer_*_SKIM",
     "drop *_recoTauPileUpVertices_*_SKIM",
-    "drop *_towerMaker_*_*", #HCAL towers
+    "drop *_towerMaker_*_*",
     "drop *_particleFlowClusterHF*_*_*",
     "drop *_pfPhotonTranslator_*_*",
     "drop *_pfElectronTranslator_*_*",
     "drop *_correctedHybridSuperClusters_*_*",
     "drop *_correctedMulti5x5SuperClustersWithPreshower_*_*",
+    "drop *_*Voronoi*_*_*"
     ),
     fileName = cms.untracked.string(
     'Summer12_DYToTauTau_skim.root'
@@ -145,6 +116,5 @@ process.output = cms.OutputModule(
     )
 
 process.p = cms.Path(process.PFTau*process.HadronicTauDecayFinder)
-## process.p = cms.Path(process.HadronicTauDecayFinder)
 
 process.end = cms.EndPath(process.output)
