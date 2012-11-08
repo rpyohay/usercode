35acfed9d9e1b1ce3d4d75458c3a2a72744abe3b
yohay <>
2012-11-08 16:53:44 +0000

    made more configurable

--- BoostedTauAnalysis/TauAnalyzer/test/combineResults.sh
+++ BoostedTauAnalysis/TauAnalyzer/test/combineResults.sh
@@ -5,11 +5,17 @@ discriminator=$1
 sample=$2
 minJob=$3
 maxJob=$4
+#filePrefix=$5
+#suffix=$6
 
 #macros
-filePrefix="${sample}_${discriminator}/analyzeSelectionTemplate_${discriminator}_"
+#filePrefix="${sample}_${discriminator}/analyzeSelectionTemplate_${discriminator}_"
+filePrefix="../../TauSkimmer/test/Summer12_WToMuNu_skim/res/CMSSW_"
+suffix=".stdout"
+#suffix=".txt"
 
 #totals
+nProcessedTot=0
 nWMuNuTot=0
 nHLTTot=0
 nWMuonPTTot=0
@@ -25,34 +31,42 @@ for iJob in `seq $minJob $maxJob`
   echo "Job #$iJob"
 
   #get the numbers of events passing each cut
-  nWMuNu=`grep genWMuNuSelector ${filePrefix}${iJob}.txt | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
-  nHLT=`grep IsoMu24eta2p1Selector ${filePrefix}${iJob}.txt | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
-  nWMuonPT=`grep WMuonPTSelector ${filePrefix}${iJob}.txt | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
-  nWMuonIso=`grep WIsoMuonSelector ${filePrefix}${iJob}.txt | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
-  nJet=`grep jetSelector ${filePrefix}${iJob}.txt | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
-  nTauMuonPT=`grep tauMuonPTSelector ${filePrefix}${iJob}.txt | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
-  nTauMuonSoft=`grep tauMuonSelector ${filePrefix}${iJob}.txt | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
-  nMuHadIso=`grep muHadIsoTauSelector ${filePrefix}${iJob}.txt | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
+  nProcessed=`grep genWMuNuSelector ${filePrefix}${iJob}${suffix} | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*\([0-9]*\)[ ]*[0-9]*.*%\1%"`
+  nWMuNu=`grep genWMuNuSelector ${filePrefix}${iJob}${suffix} | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
+  nHLT=`grep IsoMu24eta2p1Selector ${filePrefix}${iJob}${suffix} | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
+  nWMuonPT=`grep WMuonPTSelector ${filePrefix}${iJob}${suffix} | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
+  nWMuonIso=`grep WIsoMuonSelector ${filePrefix}${iJob}${suffix} | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
+  nJet=`grep jetSelector ${filePrefix}${iJob}${suffix} | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
+  nTauMuonPT=`grep tauMuonPTSelector ${filePrefix}${iJob}${suffix} | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
+  nTauMuonSoft=`grep tauMuonSelector ${filePrefix}${iJob}${suffix} | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
+  nMuHadIso=`grep muHadIsoTauSelector ${filePrefix}${iJob}${suffix} | head -n 1 | sed -e "s%TrigReport[ ]*1[ ]*0[ ]*[0-9]*[ ]*\([0-9]*\).*%\1%"`
 
   #increment totals
-  nWMuNuTot=`expr $nWMuNuTot + $nWMuNu`
-  nHLTTot=`expr $nHLTTot + $nHLT`
-  nWMuonPTTot=`expr $nWMuonPTTot + $nWMuonPT`
-  nWMuonIsoTot=`expr $nWMuonIsoTot + $nWMuonIso`
-  nJetTot=`expr $nJetTot + $nJet`
-  nTauMuonPTTot=`expr $nTauMuonPTTot + $nTauMuonPT`
-  nTauMuonSoftTot=`expr $nTauMuonSoftTot + $nTauMuonSoft`
-  nMuHadIsoTot=`expr $nMuHadIsoTot + $nMuHadIso`
+  #jobs 710, 843, and 850 are status 0 in CRAB but no file can be found for them
+  #job 173 is done but wasn't included in run over files in my EOS area
+  if [ "$nWMuNu" != "" ] && [ "$nHLT" != "" ] && [ "$nWMuonPT" != "" ] && [ "$nWMuonIso" != "" ] && [ $iJob -ne 70 ] && [ $iJob -ne 135 ] && [ $iJob -ne 173 ] && [ $iJob -ne 251 ] && [ $iJob -ne 326 ] && [ $iJob -ne 390 ] && [ $iJob -ne 501 ] && [ $iJob -ne 546 ] && [ $iJob -ne 568 ] && [ $iJob -ne 710 ] && [ $iJob -ne 769 ] && [ $iJob -ne 835 ] && [ $iJob -ne 836 ] && [ $iJob -ne 843 ] && [ $iJob -ne 846 ] && [ $iJob -ne 850 ] && [ $iJob -ne 892 ] && [ $iJob -ne 1131 ]
+      then
+      nProcessedTot=`expr $nProcessedTot + $nProcessed`
+      nWMuNuTot=`expr $nWMuNuTot + $nWMuNu`
+      nHLTTot=`expr $nHLTTot + $nHLT`
+      nWMuonPTTot=`expr $nWMuonPTTot + $nWMuonPT`
+      nWMuonIsoTot=`expr $nWMuonIsoTot + $nWMuonIso`
+  fi
+#  nJetTot=`expr $nJetTot + $nJet`
+#  nTauMuonPTTot=`expr $nTauMuonPTTot + $nTauMuonPT`
+#  nTauMuonSoftTot=`expr $nTauMuonSoftTot + $nTauMuonSoft`
+#  nMuHadIsoTot=`expr $nMuHadIsoTot + $nMuHadIso`
 done
 
 #print totals
+echo "nProcessedTot = $nProcessedTot"
 echo "nWMuNuTot = $nWMuNuTot"
 echo "nHLTTot = $nHLTTot"
 echo "nWMuonPTTot = $nWMuonPTTot"
 echo "nWMuonIsoTot = $nWMuonIsoTot"
-echo "nJetTot = $nJetTot"
-echo "nTauMuonPTTot = $nTauMuonPTTot"
-echo "nTauMuonSoftTot = $nTauMuonSoftTot"
-echo "nMuHadIsoTot = $nMuHadIsoTot"
+#echo "nJetTot = $nJetTot"
+#echo "nTauMuonPTTot = $nTauMuonPTTot"
+#echo "nTauMuonSoftTot = $nTauMuonSoftTot"
+#echo "nMuHadIsoTot = $nMuHadIsoTot"
 
 exit 0
