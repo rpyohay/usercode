661d40a647a77d65ba63d9a4f0c8a9f1a6e9c27b
yohay <>
2012-11-08 16:55:18 +0000

    for running analysis over large EOS dataset

---
+++ BoostedTauAnalysis/TauAnalyzer/test/tauanalyzer_template_cfg.py
@@ -0,0 +1,133 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MUHADANALYSIS")
+
+#PDG IDs
+A_PDGID = 36
+Z_PDGID = 23
+W_PDGID = 24
+TAU_PDGID = 15
+MU_PDGID = 13
+NUMU_PDGID = 14
+D_PDGID = 1
+U_PDGID = 2
+S_PDGID = 3
+C_PDGID = 4
+B_PDGID = 5
+T_PDGID = 6
+G_PDGID = 21
+ANY_PDGID = 0
+
+#tau decay types
+TAU_HAD = 0
+TAU_MU = 1
+TAU_E = 2
+TAU_ALL = 3
+
+#tau hadronic decay types
+TAU_ALL_HAD = -1
+TAU_1PRONG_0NEUTRAL = 0
+TAU_1PRONG_1NEUTRAL = 1
+TAU_1PRONG_2NEUTRAL = 2
+TAU_1PRONG_3NEUTRAL = 3
+TAU_1PRONG_NNEUTRAL = 4
+TAU_2PRONG_0NEUTRAL = 5
+TAU_2PRONG_1NEUTRAL = 6
+TAU_2PRONG_2NEUTRAL = 7
+TAU_2PRONG_3NEUTRAL = 8
+TAU_2PRONG_NNEUTRAL = 9
+TAU_3PRONG_0NEUTRAL = 10
+TAU_3PRONG_1NEUTRAL = 11
+TAU_3PRONG_2NEUTRAL = 12
+TAU_3PRONG_3NEUTRAL = 13
+TAU_3PRONG_NNEUTRAL = 14
+TAU_RARE = 15
+
+#no consideration of pT rank
+ANY_PT_RANK = -1
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+readFiles = cms.untracked.vstring()
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = readFiles,
+    skipEvents = cms.untracked.uint32(0)
+    )
+readFiles.extend([
+    FILES
+    ])
+
+#for L1GtStableParametersRcd and jet corrections
+#START52_V9B is recommended for JEC in Summer12 CMSSWv5.2 MC
+#START52_V9 is what the Summer12 CMSSWv5.2 MC was produced with
+#START52_V9B = START52_V9 + latest JEC
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = cms.string('START52_V9B::All')
+
+#for HLT selection
+process.load('HLTrigger/HLTfilters/hltHighLevel_cfi')
+
+#for mu-less jets
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.GeometryExtended_cff")
+process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
+process.load("RecoTauTag.RecoTau.RecoTauPiZeroProducer_cfi")
+process.load('BoostedTauAnalysis/CleanJets/cleanjets_cfi')
+
+#for jet corrections
+process.load('JetMETCorrections.Configuration.JetCorrectionServices_cff')
+
+#define a parameter set to be passed to all modules that utilize GenTauDecayID for signal taus
+commonGenTauDecayIDPSet = cms.PSet(momPDGID = cms.vint32(A_PDGID),
+                                   chargedHadronPTMin = cms.double(0.0),
+                                   neutralHadronPTMin = cms.double(0.0),
+                                   chargedLeptonPTMin = cms.double(0.0),
+                                   totalPTMin = cms.double(0.0))
+
+#produce gen parton collection
+process.genPartonSelector = cms.EDFilter(
+    'GenPartonProducer',
+    genParticleTag = cms.InputTag('genParticles'),
+    partonAbsEtaMax = cms.double(-1.0),
+    partonPTMin = cms.double(-1.0),
+    minNumGenObjectsToPassFilter = cms.uint32(1)
+    )
+
+#produce AK5 PF jets
+process.recoJetSelector = cms.EDFilter(
+    'PFJetRefSelector',
+    src = cms.InputTag('ak5PFJets'),
+    cut = cms.string('abs(eta) < 2.4'),
+    filter = cms.bool(True)
+    )
+
+#analyze gen-matched AK5 PF jets
+process.analyzeJets = cms.EDAnalyzer(
+    'JetAnalyzer',
+    outFileName = cms.string('OUTPUTROOTFILE'),
+    textOutFileName = cms.string('OUTPUTTEXTFILE'),
+    genTauDecayIDPSet = commonGenTauDecayIDPSet,
+    applyPTCuts = cms.bool(False),
+    countKShort = cms.bool(True),
+    sisterAbsMatchPDGID = cms.uint32(ANY_PDGID),
+    jetTags = cms.VInputTag('recoJetSelector'),
+    METTag = cms.InputTag('pfMet'),
+    genParticleTag = cms.InputTag('genParticles'),
+    selectedGenObjTag = cms.InputTag('genPartonSelector'),
+    muonTag = cms.InputTag('WIsoMuonSelector'),
+    dR = cms.double(0.4),
+    doGenTauFinding = cms.bool(False),
+    pTRankColors = cms.vuint32(1, 2, 4, 6),
+    pTRankStyles = cms.vuint32(20, 21, 22, 23),
+    pTRankEntries = cms.vstring('Highest p_{T}', 'Second highest p_{T}', 'Third highest p_{T}',
+                                'Lowest p_{T}')
+    )
+
+#path
+process.p = cms.Path(process.genPartonSelector*process.recoJetSelector*process.analyzeJets)
