97996af05d0b7a117b6ca67cd6c644b9698a4f70
yohay <>
2012-11-30 10:15:26 +0000

    updated to print trigger menu for 1 CMSSWv5.3 MC event

--- TriggerAnalysis/TriggerAnalyzer/triggeranalyzer_cfg.py
+++ TriggerAnalysis/TriggerAnalyzer/triggeranalyzer_cfg.py
@@ -10,18 +10,18 @@ process.load('Configuration/StandardSequences/EndOfProcess_cff')
 
 #global tag: needed for trigger stuff
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = cms.string('GR_P_V14::All')
+process.GlobalTag.globaltag = cms.string('START53_V7F::All')
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-    'file:/data2/yohay/diPhoton_skim_Run2011A_30GeV-30GeV_prelimAnalysis.root'
+    'file:/data1/yohay/W1JetsToLNu_TuneZ2Star_8TeV-madgraph-Summer12_DR53X-PU_S10_START53_V7A-v1-AODSIM.root'
     )
                             )
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
 
 process.load('TriggerAnalysis/TriggerAnalyzer/triggeranalyzer_cfi')
 process.TriggerAnalyzer.outputFile = cms.untracked.string(
-    '/data2/yohay/diPhoton_skim_Run2011A_30GeV-30GeV_trgAnalysis.root'
+    'dummy.root'
     )
 process.TriggerAnalyzer.unprescaledHLTPaths = cms.untracked.vstring(
     'HLT_Photon32_CaloIdL_Photon26_CaloIdL'
