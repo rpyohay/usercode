8628a0392d20d8eab9ca817f571aff39e6e2719e
yohay <>
2012-12-06 17:42:16 +0000

    added more modules

--- BoostedTauAnalysis/TauAnalyzer/test/tauanalyzer_cfg.py
+++ BoostedTauAnalysis/TauAnalyzer/test/tauanalyzer_cfg.py
@@ -60,15 +60,14 @@ process.source = cms.Source(
     skipEvents = cms.untracked.uint32(0)
     )
 readFiles.extend([
-##     'file:/data1/yohay/Wh1_Medium/Wh1_Medium_selected_events_0.root'
-    'file:/data1/yohay/WJets_Medium/WJets_Medium_selected_events_job1.root',
-    'file:/data1/yohay/WJets_Medium/WJets_Medium_selected_events_job2.root'
+    #need to make W skim of sig w/o decay mode finding selection
     ])
 
 #for L1GtStableParametersRcd and jet corrections
 #START52_V9B is recommended for JEC in Summer12 CMSSWv5.2 MC
 #START52_V9 is what the Summer12 CMSSWv5.2 MC was produced with
 #START52_V9B = START52_V9 + latest JEC
+#START52_V9B is what is also recommended for CMSSWv5.3 MC as of 30-Nov-12
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.GlobalTag.globaltag = cms.string('START52_V9B::All')
 
@@ -77,7 +76,7 @@ process.load('HLTrigger/HLTfilters/hltHighLevel_cfi')
 
 #for mu-less jets
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
-process.load("Configuration.StandardSequences.GeometryExtended_cff")
+process.load("Configuration.Geometry.GeometryRecoDB_cff")
 process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
 process.load("RecoTauTag.RecoTau.RecoTauPiZeroProducer_cfi")
 process.load('BoostedTauAnalysis/CleanJets/cleanjets_cfi')
@@ -94,24 +93,114 @@ process.genPartonSelector = cms.EDFilter(
     minNumGenObjectsToPassFilter = cms.uint32(0)
     )
 
-#analyze selected taus
+#clean the jets of soft muons, then rebuild the taus
+process.CleanJets.muonSrc = cms.InputTag('tauMuonSelector')
+process.CleanJets.PFCandSrc = cms.InputTag('particleFlow')
+process.CleanJets.cutOnGenMatches = cms.bool(False)
+process.CleanJets.outFileName = cms.string('NMSSMSignal_MuProperties_W1JetsToLNu.root')
+process.recoTauAK5PFJets08Region.src = cms.InputTag("CleanJets", "ak5PFJetsNoMu", "MUHADANALYSIS")
+process.recoTauAK5PFJets08Region.jetMuonMapTag = cms.InputTag("CleanJets", "", "MUHADANALYSIS")
+process.ak5PFJetsRecoTauPiZeros.jetSrc = cms.InputTag("CleanJets", "ak5PFJetsNoMu",
+                                                      "MUHADANALYSIS")
+process.combinatoricRecoTaus.jetSrc = cms.InputTag("CleanJets", "ak5PFJetsNoMu", "MUHADANALYSIS")
+process.ak5PFJetTracksAssociatorAtVertex.jets = cms.InputTag("CleanJets", "ak5PFJetsNoMu",
+                                                             "MUHADANALYSIS")
+process.ak5PFJetsLegacyHPSPiZeros.jetSrc = cms.InputTag("CleanJets", "ak5PFJetsNoMu",
+                                                        "MUHADANALYSIS")
+process.recoTauCommonSequence = cms.Sequence(process.CleanJets*
+                                             process.ak5PFJetTracksAssociatorAtVertex*
+                                             process.recoTauAK5PFJets08Region*
+                                             process.recoTauPileUpVertices*
+                                             process.pfRecoTauTagInfoProducer
+                                             )
+process.PFTau = cms.Sequence(process.recoTauCommonSequence*process.recoTauClassicHPSSequence)
+
+#find taus in |eta| < 2.4 matched to muon-tagged cleaned jets that pass the isolation
+#discriminator
+#this will produce a ref to the cleaned tau collection
+process.muHadIsoTauSelector = cms.EDFilter(
+    'CustomTauSelector',
+    baseTauTag = cms.InputTag('hpsPFTauProducer', '', 'MUHADANALYSIS'),
+    tauDiscriminatorTags = cms.VInputTag(
+    cms.InputTag('hpsPFTauDiscriminationByDecayModeFinding', '', 'MUHADANALYSIS'), 
+    cms.InputTag('hpsPFTauDiscriminationByMediumCombinedIsolationDBSumPtCorr', '', 'MUHADANALYSIS')
+    ),
+    jetTag = cms.InputTag('CleanJets', 'ak5PFJetsNoMu', 'MUHADANALYSIS'),
+    muonRemovalDecisionTag = cms.InputTag('CleanJets'),
+    muonTag = cms.InputTag('WIsoMuonSelector'),
+    passDiscriminator = cms.bool(True),
+    etaMax = cms.double(2.4),
+    dR = cms.double(0.5),
+    minNumObjsToPassFilter = cms.uint32(0)
+    )
+
+#find taus in |eta| < 2.4 matched to muon-tagged cleaned jets that pass decay mode finding
+#this will produce a ref to the cleaned tau collection
+process.muHadTauSelector = process.muHadIsoTauSelector.clone()
+process.muHadTauSelector.tauDiscriminatorTags = cms.VInputTag(
+    cms.InputTag('hpsPFTauDiscriminationByDecayModeFinding', '', 'MUHADANALYSIS')
+    )
+
+#find taus in |eta| < 2.4 matched to muon-tagged cleaned jets that fail the isolation
+#discriminator
+#this will produce a ref to the cleaned tau collection
+process.muHadNonIsoTauSelector = process.muHadIsoTauSelector.clone()
+process.muHadNonIsoTauSelector.tauTag = cms.InputTag('muHadTauSelector')
+process.muHadNonIsoTauSelector.tauDiscriminatorTags = cms.VInputTag(
+    cms.InputTag('hpsPFTauDiscriminationByMediumCombinedIsolationDBSumPtCorr', '', 'MUHADANALYSIS')
+    )
+process.muHadNonIsoTauSelector.passDiscriminator = cms.bool(False)
+
+#analyze isolated taus
 process.muHadIsoTauAnalyzer = cms.EDAnalyzer(
     'TauAnalyzer',
-##     outFileName = cms.string('/data1/yohay/Wh1_Medium/muHadAnalysisV3.root'),
-    outFileName = cms.string('/data1/yohay/WJets_Medium/muHadAnalysisV4.root'),
+    outFileName = cms.string(
+    '/data1/yohay/WNJetsToLNu/analysis/muHadIsoAnalysis_W1JetsToLNu_v4.root'
+    ),
     tauTag = cms.InputTag('muHadIsoTauSelector'),
     METTag = cms.InputTag('pfMet'),
     muonTag = cms.InputTag('WIsoMuonSelector'),
+    genMatchedMuonTag = cms.InputTag('WIsoMuonSelector'),
     oldJetTag = cms.InputTag('ak5PFJets'),
+    newJetTag = cms.InputTag('CleanJets', 'ak5PFJetsNoMu', 'MUHADANALYSIS'),
     jetMuonMapTag = cms.InputTag('CleanJets'),
     oldNewJetMapTag = cms.InputTag('CleanJets'),
     genParticleTag = cms.InputTag('genPartonSelector'),
-    dR = cms.double(0.4),
+    tauHadIsoTag = cms.InputTag('hpsPFTauDiscriminationByRawCombinedIsolationDBSumPtCorr', '',
+                                'MUHADANALYSIS'),
+    dR = cms.double(0.3),
+    tauPTMin = cms.double(0.0), #GeV
+    tauDecayMode = cms.int32(TAU_ALL_HAD),
     pTRankColors = cms.vuint32(1, 2, 4, 6),
     pTRankStyles = cms.vuint32(20, 21, 22, 23),
     pTRankEntries = cms.vstring('Highest p_{T}', 'Second highest p_{T}', 'Third highest p_{T}',
                                 'Lowest p_{T}')
     )
 
+#analyze non-isolated taus
+process.muHadNonIsoTauAnalyzer = process.muHadIsoTauAnalyzer.clone()
+process.muHadNonIsoTauAnalyzer.outFileName = cms.string(
+    '/data1/yohay/WNJetsToLNu/analysis/muHadNonIsoAnalysis_W1JetsToLNu_v4.root'
+    )
+process.muHadNonIsoTauAnalyzer.tauTag = cms.InputTag('muHadNonIsoTauSelector')
+
+#output
+process.output = cms.OutputModule(
+    "PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('p')),
+    fileName = cms.untracked.string(
+    '/data1/yohay/WNJetsToLNu/EDM_files/W1JetsToLNu_softMuIsoCandsExcluded.root'
+    )
+    )
+
+#sequences
+process.isoTauAnalysisSequence = cms.Sequence(process.muHadIsoTauSelector*
+                                              process.muHadIsoTauAnalyzer)
+process.nonIsoTauAnalysisSequence = cms.Sequence(process.muHadTauSelector*
+                                                 process.muHadNonIsoTauSelector*
+                                                 process.muHadNonIsoTauAnalyzer)
+
 #path
-process.p = cms.Path(process.genPartonSelector*process.muHadIsoTauAnalyzer)
+process.p = cms.Path(process.genPartonSelector*process.PFTau*process.isoTauAnalysisSequence*
+                     process.nonIsoTauAnalysisSequence)
+process.e = cms.EndPath(process.output)
